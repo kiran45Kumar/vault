4933c152f984fd5bef65826c36e1e423e3fface0
kiran45kumar <kirankiccha2003@gmail.com>
2026-05-01 14:34:27 +0530

    added ui

--- vault_project/vault_project/settings.py
+++ vault_project/vault_project/settings.py
@@ -104,12 +104,12 @@ WSGI_APPLICATION = "vault_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "OPTIONS": {"sslmode": "require"},
     }
 }
 
